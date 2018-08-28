0c83e7d8d5f38944e75c4d54b6b0776b82149c14
Yosia <yosia@tiket.com>
2018-08-28 23:42:51 +0700

    add controller generator

--- main.go
+++ main.go
@@ -17,8 +17,66 @@ func main(){
 
 	stringsArray := strings.Split(str,"\n")
 
-	GenerateRepository("SystemParameter", "com.tiket.tix.flight.core")
-	GenerateFieldsConstant("SystemParameter", "com.tiket.tix.flight.core", stringsArray)
+	className := "SystemParameter"
+	packageName := "com.tiket.tix.flight.core"
+	classNameLowerFirst := "systemParameter"
+	classNameTitle := "System Parameter"
+	classNameConstant := "SYSTEM_PARAMETER"
+
+
+	GenerateRepository(className, packageName)
+	GenerateFieldsConstant(className, packageName, stringsArray)
+	GenerateController(className, packageName, classNameLowerFirst, classNameTitle, classNameConstant)
+	GenerateServiceApi(className, packageName)
+	GenerateServiceImpl(className, packageName, classNameLowerFirst)
+
+}
+func GenerateServiceImpl(className string, packageName string, classNameLowerFirst string) {
+	b, err := ioutil.ReadFile("templates/service-impl/service_impl_template.txt") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	contents := string(b)
+
+	output := strings.Replace(contents,"[className]", className, -1)
+	output = strings.Replace(output,"[packageName]", packageName, -1)
+	output = strings.Replace(output,"[classNameLowerFirst]", classNameLowerFirst, -1)
+
+
+	fmt.Println(output)
+}
+func GenerateController(className string, packageName string, classNameLowerFirst string, classNameTitle string, classNameConstant string) {
+	b, err := ioutil.ReadFile("templates/rest-web/controller/controller_template.txt") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	contents := string(b)
+
+	output := strings.Replace(contents,"[className]", className, -1)
+	output = strings.Replace(output,"[packageName]", packageName, -1)
+	output = strings.Replace(output,"[classNameLowerFirst]", classNameLowerFirst, -1)
+	output = strings.Replace(output,"[classNameTitle]", classNameTitle, -1)
+	output = strings.Replace(output,"[classNameConstant]", classNameConstant, -1)
+
+
+	fmt.Println(output)
+}
+
+func GenerateServiceApi(className string, packageName string) {
+	b, err := ioutil.ReadFile("templates/service-api/service_api_template.txt") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	contents := string(b)
+
+	output := strings.Replace(contents,"[className]", className, -1)
+	output = strings.Replace(output,"[packageName]", packageName, -1)
+
+
+	fmt.Println(output)
 }
 
 func GenerateRepository(className string, packageName string) {
